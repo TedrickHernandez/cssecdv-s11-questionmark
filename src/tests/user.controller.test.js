68fbbda3e8eda87509b529c01f98a12b5b482aec
stanpai78 <stanpai78@gmail.com>
2024-02-14 17:04:38 +0800

    Refactor: added default email, password, and user to tests

--- src/tests/user.controller.test.js
+++ src/tests/user.controller.test.js
@@ -3,15 +3,18 @@ const request = require('supertest')
 
 const baseURL = 'http://localhost:' + process.env.PORT
 
+const defaultEmail = 'standesu@email.com'
+const defaultPassword = '0123456789abcdef'
+const defaultUser = {
+    first_name: 'stan',
+    last_name: 'desu',
+    email: defaultEmail,
+    password: defaultPassword,
+    number: '0917 123 4567'
+}
+
 test('user creates account with valid information provided and no photo', async () => {
-    const newUser = {
-        first_name: 'stan',
-        last_name: 'desu',
-        email: 'standesu@email.com',
-        password: '0123456789abcdef',
-        number: '0917 123 4567'
-    }
-    const createUser = await request(baseURL).post('/api/createUser').send(newUser);
+    const createUser = await request(baseURL).post('/api/createUser').send(defaultUser);
     expect(createUser.statusCode).toBe(201)
 });
 

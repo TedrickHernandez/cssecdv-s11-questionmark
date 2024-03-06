f2d60fd9a8068fc5260a79fc56f3b1f8e468d8bf
TedrickHernandez <tedrick_hernandez@dlsu.edu.ph>
2024-03-06 16:16:09 +0800

    Added routings to existing HTML pages (VERY UNSECURE)

--- src/routes/router.js
+++ src/routes/router.js
@@ -23,6 +23,22 @@ router.get('/home', (req, res) => {
     res.sendFile(path.join(__dirname, '../../views', 'home.html'));
 })
 
+router.get('/userDashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views', 'userDashboard.html'));
+})
+
+router.get('/viewProfile', (req, res) => { // ATTENTION: FOR TESTING PURPOSES ONLY!!!
+    res.sendFile(path.join(__dirname, '../../views', 'profile.html'));
+})
+
+router.get('/editProfile', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views', 'editProfile.html'));
+})
+
+router.get('/settings', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../views', 'userSettings.html'));
+})
+
 router.post('/register', multer.upload.single('profilePhoto'), usersController.createUser)
 
 // Logout Endpoint

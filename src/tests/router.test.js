3e3fa09e83fdbc73283ad70cea7efcd29801675f
stanpai78 <stanpai78@gmail.com>
2024-03-20 22:06:24 +0800

    fix tests for https

--- src/tests/router.test.js
+++ src/tests/router.test.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const request = require('supertest')
 
-const baseURL = 'http://localhost:' + process.env.PORT
+const baseURL = 'https://localhost:443'
 
 test('user opens website with no directory should be redirected to /home', async () => {
-    const res = await request(baseURL).get('/');
+    const res = await request(baseURL).get('/').trustLocalhost(true);
     expect(res.statusCode).toBe(302)
     expect(res.redirect).toBe(true);
     expect(res.headers['location']).toBe('/home')
 })
 
 test('user opens admin panel but should be redirected to /home', async () => {
-    const res = await request(baseURL).get('/admin');
+    const res = await request(baseURL).get('/admin').trustLocalhost(true);
     expect(res.statusCode).toBe(302)
     expect(res.redirect).toBe(true);
     expect(res.headers['location']).toBe('/')
--- src/tests/user.controller.test.js
+++ src/tests/user.controller.test.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const request = require('supertest')
 
-const baseURL = 'http://localhost:' + process.env.PORT
+const baseURL = 'https://localhost:443'
 
 const defaultEmail = 'standesu@email.com'
 const defaultPassword = '0123456789abcdef'
@@ -12,9 +12,6 @@ const defaultUser = {
     password: defaultPassword,
     phoneNumber: '0917 123 4567'
 }
-beforeAll(async () => {
-    jest.setTimeout(100000); // change timeout to 10 seconds
-});
 
 test('user creates account with valid information provided and no photo', async () => {
     const createUser = await request(baseURL).post('/api/createUser').send(defaultUser);
@@ -54,7 +51,7 @@ test('user creates account w/o photo but email already in use', async () => {
     expect(createUser1.statusCode).toBe(302);
     const createUser2 = await request(baseURL).post('/api/createUser').send(newUser2);
     expect(createUser2.statusCode).toBe(302);
-}, 10000);
+});
 
 test('admin verifies with correct email and password', async () => {
     const verifyUser = await request(baseURL).post('/api/verifyUser').send({ email: defaultEmail, password: defaultPassword })

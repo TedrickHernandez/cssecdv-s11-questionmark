72939d9a065bb42756ef3c0eaaaae5a1e28e35b8
stanpai78 <stanpai78@gmail.com>
2024-03-21 01:09:36 +0800

    update sql generate

--- GENERATE.sql
+++ GENERATE.sql
@@ -43,7 +43,7 @@ CREATE TABLE cssecdb.`friends` (
 INSERT INTO cssecdb.`roles`(`email`,`isAdmin`) VALUES('standesu@email.com', 1);
 
 create user cssecdv@localhost identified by 'extremelySecurePassword';
-grant select, insert, update on cssecdb.sessions to cssecdv@localhost;
+grant select, insert, update, delete on cssecdb.sessions to cssecdv@localhost;
 grant select, insert, update, delete on cssecdb.users to cssecdv@localhost;
 grant select, insert, update, delete on cssecdb.friends to cssecdv@localhost;
 grant select, insert, update on cssecdb.roles to cssecdv@localhost;

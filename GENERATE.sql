82faf233eb15ee367978cb697e14a670c6815d5b
stanpai78 <stanpai78@gmail.com>
2024-03-20 17:57:39 +0800

    update sql generate

--- GENERATE.sql
+++ GENERATE.sql
@@ -1,11 +1,15 @@
-CREATE TABLE `milestone1.users` (
+drop schema if exists cssecdb;
+create schema if not exists cssecdb;
+
+CREATE TABLE cssecdb.`users` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `email` varchar(320) NOT NULL,
   `password` varchar(255) NOT NULL,
   `number` varchar(255) NOT NULL,
-  `photo` longblob,
+  `photo` varchar(255),
+  `poked` boolean default False,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
@@ -13,26 +17,26 @@ CREATE TABLE `milestone1.users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `milestone1.sessions` (
+CREATE TABLE cssecdb.`sessions` (
   `id` varchar(32) NOT NULL,
   `email` varchar(320) NOT NULL,
   `expiresOn` datetime NOT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
+  PRIMARY KEY (`email`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `milestone1.roles` (
+CREATE TABLE cssecdb.`roles` (
   `email` varchar(320) NOT NULL,
   `isAdmin` tinyint NOT NULL DEFAULT '0',
   PRIMARY KEY (`email`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-create user cssecdv@localhost identified by 'extremelySecurePassword';
-grant select, insert, update on milestone1.sessions to cssecdv@localhost;
-grant select, insert, update on milestone1.users to cssecdv@localhost;
-grant select on miletsone1.roles to cssecdv@localhost;
+INSERT INTO cssecdb.`roles`(`email`,`isAdmin`) VALUES('standesu@email.com', 1);
 
-INSERT INTO `milestone1`.`roles`(`email`,`isAdmin`) VALUES('standesu@email.com', 1);
+create user cssecdv@localhost identified by 'extremelySecurePassword';
+grant select, insert, update on cssecdb.sessions to cssecdv@localhost;
+grant select, insert, update on cssecdb.users to cssecdv@localhost;
+grant select on cssecdb.roles to cssecdv@localhost;
